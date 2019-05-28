109dca28ba6eea75c4b19f08c0272ee900175eab
jesse <xtso520ok@gmail.com>
2019-05-28 13:03:54 +0800

    add baidu/tongji

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,18 @@ prepare(){
 cat README.html | \
   sed -e 's/fonts.googleapis.com\/css/www.8ops.top\/asset\/index.css/g' \
       -e 's/file:\/\/.*github/asset/g' > index.html
+
+cat >> index.html <<EOF
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b4169476d6bbeb09748f80aca3fa4d2e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+EOF
 }
 
 deploy(){
